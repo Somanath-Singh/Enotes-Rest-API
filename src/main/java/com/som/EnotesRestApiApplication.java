4945fefcb3a7e2525eaba95e1507b2c2ec43528f
Somanath Singh <somanathsingh531@gmail.com>
2025-01-07 11:35:58 +0530

    Commit the code

---
+++ src/main/java/com/som/EnotesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.som;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesRestApiApplication.class, args);
+	}
+
+}
